af520273f01f091f4f099f455b9b8620f24170ef
LegoKirbyMan <timofei.sadownikov@yandex.ru>
2025-01-20 11:35:40 +0300

    Initial Commit

---
+++ src/main/java/ru/netology/MovieMan.java
@@ -0,0 +1,42 @@
+package ru.netology;
+
+import java.util.Arrays;
+
+public class MovieMan {
+
+    private String[] movieList;
+    private int findLastLength;
+
+    public MovieMan(int findLastLength) {
+        this.findLastLength = findLastLength;
+    }
+
+    public MovieMan() {
+        this(5);
+    }
+
+    public String[] getList() {
+        return movieList;
+    }
+    
+    public void addMovie(String title) {
+        if (movieList != null) {
+            String[] tmp = new String[movieList.length + 1];
+            System.arraycopy(movieList, 0, tmp, 0, movieList.length);
+            tmp[tmp.length - 1] = title;
+            movieList = tmp;
+        } else {
+            movieList = new String[1];
+            movieList[0] = title;
+        }
+    }
+
+    public String findAll() {
+        return Arrays.toString(movieList);
+    }
+
+    public String findLast() {
+        String[] tmp = java.util.Arrays.copyOfRange(movieList, movieList.length - findLastLength, movieList.length);
+        return Arrays.toString(tmp);
+    }
+}
---
+++ src/test/java/ru/netology/MovieManTest.java
@@ -0,0 +1,60 @@
+package ru.netology;
+
+import java.util.Arrays;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MovieManTest {
+    private String[] testMovieList = {
+        "Бладшот",
+        "Вперёд",
+        "Отель Белград",
+        "Джентельмены",
+        "Человек-Невидимка",
+        "Тролли. Мирововой тур"
+    };
+    
+    @Test
+    void addMovieTest() {
+        MovieMan manager = new MovieMan();
+        for (int i=0; i<testMovieList.length; i++){
+            manager.addMovie(testMovieList[i]);
+        }
+
+        Assertions.assertArrayEquals(testMovieList, manager.getList());
+    }
+
+    @Test
+    void findAllTest() {
+        MovieMan manager = new MovieMan();
+        for (int i=0; i<testMovieList.length; i++){
+            manager.addMovie(testMovieList[i]);
+        }
+
+        Assertions.assertEquals(Arrays.toString(testMovieList), manager.findAll());
+    }
+
+    @Test
+    void findLastTest() {
+        MovieMan manager = new MovieMan();
+        for (int i=0; i<testMovieList.length; i++){
+            manager.addMovie(testMovieList[i]);
+        }
+
+        String[] expected = java.util.Arrays.copyOfRange(testMovieList, 1, 6);
+        String actual = manager.findLast();
+
+        Assertions.assertEquals(Arrays.toString(expected), actual);
+
+        MovieMan manager2 = new MovieMan(3);
+        for (int i=0; i<testMovieList.length; i++){
+            manager2.addMovie(testMovieList[i]);
+        }
+
+        expected = java.util.Arrays.copyOfRange(testMovieList, 3, 6);
+        actual = manager2.findLast();
+
+        Assertions.assertEquals(Arrays.toString(expected), actual);
+    }
+}
